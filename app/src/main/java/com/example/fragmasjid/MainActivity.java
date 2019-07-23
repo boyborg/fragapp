001e8f52917f1f5893383ec98e8d2eea589abb82
Raka Dean <rakache@gmail.com>
2019-07-23 14:22:36 +0700

    second commit

--- app/src/main/java/com/example/fragmasjid/MainActivity.java
+++ app/src/main/java/com/example/fragmasjid/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.fragmasjid;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
 
     @Override
@@ -19,11 +22,48 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        NavigationView navigationView=(NavigationView)findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState==null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id,
+                new ProfileFrag()).commit();
+        navigationView.setCheckedItem(R.id.nav_profile);}
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id,
+                        new ProfileFrag()).commit();
+
+                break;
+            case R.id.nav_schedule:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id,
+                        new ScheduleFrag()).commit();
+
+                break;
+            case R.id.nav_sedekah:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id,
+                        new DonasiFrag()).commit();
+
+                break;
+            case R.id.nav_event:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id,
+                        new EventFrag()).commit();
+
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
